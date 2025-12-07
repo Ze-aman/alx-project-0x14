761828ef9c717452841baa882f90831f5afd108f
Zeamanuel Dida <aman09dida@gmail.com>
2025-12-07 13:47:14 +0300

    Initial commit - alx-project-0x14 - alx-movie-app project

---
+++ alx-movie-app/components/layout/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LayoutButton: React.FC = () => {
+  return (
+    <button className="px-4 py-2 bg-green-600 text-white rounded-md">
+      Layout Button
+    </button>
+  );
+};
+
+export default LayoutButton;
---
+++ alx-movie-app/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header className="w-full bg-gray-900 text-white p-4">
+      <h1 className="text-xl font-bold">MovieApp</h1>
+    </header>
+  );
+};
+
+export default Header;
